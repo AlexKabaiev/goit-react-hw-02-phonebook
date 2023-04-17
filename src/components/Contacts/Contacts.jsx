aabbb3a7aee41c65f2f6314179a736b89c24b30b
Alexey Kabaiev <kabaievalex@gmail.com>
2023-04-17 16:57:00 +0300

    Final commit

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './Contacts.module.css';
+
+const Contacts = ({ contacts, onDelete }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ name, id, number }) => {
+        return (
+          <li className={css.item} key={id}>
+            <p className={css.text}>
+              {name}: {number}
+            </p>
+            <button
+              type="button"
+              className={css.button}
+              onClick={() => onDelete(id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default Contacts;
