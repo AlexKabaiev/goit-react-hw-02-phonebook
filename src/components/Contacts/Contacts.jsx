c541625053be114e440f4b1d906accb24d5b6fb4
AlexKabaiev <kabaievalex@gmail.com>
2023-04-17 22:12:33 +0300

    commit

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -25,9 +25,13 @@ const Contacts = ({ contacts, onDelete }) => {
 };
 
 Contacts.propTypes = {
-  name: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
   onDelete: PropTypes.func.isRequired,
 };
 
